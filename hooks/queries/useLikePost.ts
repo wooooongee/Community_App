55003281c9b7e05717c4534b06d63a2ab0720c63
jaewoong <wodnd0284@naver.com>
2025-12-21 16:16:57 +0900

    Refactor: Add optimistic updates for likes

--- hooks/queries/useLikePost.ts
+++ hooks/queries/useLikePost.ts
@@ -1,17 +1,56 @@
 import { likePost } from "@/api/post";
 import queryClient from "@/api/queryClient";
 import { queryKeys } from "@/constants";
+import type { Post, Profile } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
 function useLikePost() {
   return useMutation({
     mutationFn: likePost,
-    onSuccess: (postId) => {
+    onMutate: async (postId) => {
+      await queryClient.cancelQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POST, postId],
+      });
+
+      const user = queryClient.getQueryData<Profile>([
+        queryKeys.AUTH,
+        queryKeys.GET_ME,
+      ]);
+      const userId = Number(user?.id);
+      const previousPost = queryClient.getQueryData<Post>([
+        queryKeys.POST,
+        queryKeys.GET_POST,
+        postId,
+      ]);
+
+      const newPost = { ...previousPost };
+
+      const likedIndex =
+        previousPost?.likes.findIndex((like) => like.userId === userId) ?? -1;
+
+      likedIndex >= 0
+        ? newPost.likes?.splice(likedIndex, 1)
+        : newPost.likes?.push({ userId });
+
+      queryClient.setQueryData(
+        [queryKeys.POST, queryKeys.GET_POST, postId],
+        newPost
+      );
+
+      return { previousPost, newPost };
+    },
+    onError: (err, newPost, context) => {
+      queryClient.setQueryData(
+        [queryKeys.POST, queryKeys.GET_POST, context?.previousPost?.id],
+        context?.previousPost
+      );
+    },
+    onSettled: (data, error, variables, context) => {
       queryClient.invalidateQueries({
-        queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
+        queryKey: [queryKeys.POST, queryKeys.GET_POST, variables],
       });
       queryClient.invalidateQueries({
-        queryKey: [queryKeys.POST, queryKeys.GET_POST, postId],
+        queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
       });
     },
   });
