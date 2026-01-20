20d8c2a8df9990038b5309c14e1d2382389c79c8
jaewoong <wodnd0284@naver.com>
2026-01-20 13:37:02 +0900

    Refactor: Remove unused theme properties and images
       - Remove 26+ unused theme properties (glass, aurora, sunset,
       etc.)
       - Delete glassStyle and animation exports
       - Remove unused images (android-icon-background/monochrome.png)
       - Clean up typography, spacing, radius constants

--- api/axios.ts
+++ api/axios.ts
@@ -5,8 +5,8 @@ export const baseUrls = {
   // 실제 모바일에서 시뮬레이터 돌리려면 expo start 했을때 나오는 주소 사용 ex) http://172.30.1.84:8081
   // android: "http://10.0.2.2:3030",
   // ios: "http://localhost:3030",
-  android: "http://172.30.1.7:3030",
-  ios: "http://172.30.1.7:3030",
+  android: "http://172.30.1.42:3030",
+  ios: "http://172.30.1.42:3030",
 };
 
 const axiosInstance = axios.create({
